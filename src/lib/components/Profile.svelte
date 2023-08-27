8e34fab6ee4979f8e6bbf4ddb4a96d219265da0e
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-08-28 01:12:51 +0200

    wip: profile card

--- src/lib/components/Profile.svelte
+++ src/lib/components/Profile.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { profileStore } from '$stores/profile';
 	import type { PublicProfile } from '$types/public_profile.type';
+	import { Avatar } from '@skeletonlabs/skeleton';
 
 	export let profile: PublicProfile;
 
@@ -11,10 +12,22 @@
 	};
 </script>
 
-<article
+<div
 	id="{profile.name.toLowerCase().trim().replace(' ', '-')}-profile"
 	class="flex h-full w-full flex-col items-center justify-center"
 >
-	<h1>{Object.values(profile)}</h1>
-	<button class="variant-ghost-primary btn" on:click={follow}>Follow</button>
-</article>
+	<article class="card h-4/5 w-4/5 rounded-tl-container-token rounded-tr-container-token">
+		<section
+			class="flex h-1/4 items-center justify-between bg-surface-900 p-5 rounded-tl-container-token rounded-tr-container-token"
+		>
+			<div class="flex items-center gap-4">
+				<Avatar initials={profile.name[0]} width="w-24" />
+				<div>
+					<h2 class="h2">{profile.name}</h2>
+				</div>
+			</div>
+			<button class="variant-ghost-primary btn" on:click={follow}>Follow</button>
+		</section>
+		<section class="p-3" />
+	</article>
+</div>
