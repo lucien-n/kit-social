8ba832a64cd1845d97533cf40388009ca1e5a892
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-08-27 17:53:50 +0200

    feat: update last seen

--- src/app.d.ts
+++ src/app.d.ts
@@ -6,7 +6,7 @@ declare global {
 	type SupaPost = Database['public']['Tables']['posts']['Row'];
 	declare namespace App {
 		interface Locals {
-			supabase: SupabaseClient;
+			supabase: SupabaseClient<Database>;
 			getSession(): Promise<Session | null>;
 		}
 		interface PageData {
--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -1,5 +1,12 @@
-export const load = async ({ locals: { getSession } }) => {
+export const load = async ({ locals: { getSession, supabase } }) => {
+	const session = await getSession()
+
+	if (session) {
+		const uid = session.user.id
+		await supabase.from("profiles").update({last_seen: new Date().toUTCString()}).eq("uid", uid)
+	}
+
 	return {
-		session: await getSession()
+		session
 	};
 };
