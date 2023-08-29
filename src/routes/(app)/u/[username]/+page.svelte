6c364da83dcd6f320272dd9d36ca99a7525039d6
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-08-29 17:17:18 +0200

    ref: removed useless profile component and moved it to page

--- src/routes/(app)/u/[username]/+page.svelte
+++ src/routes/(app)/u/[username]/+page.svelte
@@ -1,13 +1,28 @@
 <script lang="ts">
 	import { getProfile } from '$api/profiles';
-	import Profile from '$comp/Profile.svelte';
+	import { enhance } from '$app/forms';
+	import Avatar from '$comp/Avatar.svelte';
+	import FollowButton from '$comp/FollowButton.svelte';
+	import UploadAvatar from '$comp/UploadAvatar.svelte';
+	import { profileStore } from '$stores/profile';
 	import Icon from '@iconify/svelte';
 	import type { SupabaseClient } from '@supabase/supabase-js';
+	import type { SubmitFunction } from '@sveltejs/kit';
 
 	export let data: { supabase: SupabaseClient; error: string; username: string };
 
 	let { supabase, error, username } = data;
 	$: ({ supabase, username } = data);
+
+	let profileForm: HTMLFormElement;
+	let loading = false;
+
+	const handleSubmit: SubmitFunction = () => {
+		loading = true;
+		return async () => {
+			loading = false;
+		};
+	};
 </script>
 
 <section class="flex h-full w-full items-center justify-center">
@@ -20,7 +35,39 @@
 			</span>
 		{:then profile}
 			{#if profile}
-				<Profile {profile} {supabase} />
+				<form
+					method="post"
+					action="?/update"
+					use:enhance={handleSubmit}
+					bind:this={profileForm}
+					id="{profile.name.toLowerCase().trim().replace(' ', '-')}-profile"
+					class="flex h-full w-full flex-col items-center justify-center"
+				>
+					<article class="card h-4/5 w-4/5 rounded-tl-container-token rounded-tr-container-token">
+						<section
+							class="flex h-1/4 items-center justify-between bg-surface-900 p-5 rounded-tl-container-token rounded-tr-container-token"
+						>
+							<div class="flex items-center gap-4">
+								<div class="relative">
+									<Avatar {profile} width="w-24" />
+									{#if $profileStore?.uid == profile.uid}
+										<UploadAvatar
+											{supabase}
+											on:upload={() => {
+												profileForm.requestSubmit();
+											}}
+										/>
+									{/if}
+								</div>
+								<div>
+									<h2 class="h2">{profile.name}</h2>
+								</div>
+							</div>
+							<FollowButton {profile} />
+						</section>
+						<section class="p-3" />
+					</article>
+				</form>
 			{:else}
 				<h1 class="h1">Profile not found</h1>
 			{/if}
