43e5ae6cdaa70aa769c397faba308d9154d6a972
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-08-29 11:43:35 +0200

    switched to map for profiles store

--- src/lib/stores/profiles.ts
+++ src/lib/stores/profiles.ts
@@ -1,28 +1,29 @@
+import { checkUid } from '$lib/utils';
 import type { PublicProfile } from '$types/public_profile.type';
 import { writable, type Subscriber, type Invalidator } from 'svelte/store';
 
-type PublicProfileMap = { [uid: string]: PublicProfile };
+type PublicProfileSet = Map<string, PublicProfile>;
 
 type ProfilesStore = {
 	subscribe: (
 		this: void,
-		run: Subscriber<PublicProfileMap>,
-		invalidate?: Invalidator<PublicProfileMap>
+		run: Subscriber<PublicProfileSet>,
+		invalidate?: Invalidator<PublicProfileSet>
 	) => () => void;
-	set: (value: PublicProfileMap) => void;
-	update: (updater: (value: PublicProfileMap) => PublicProfileMap) => void;
+	set: (value: PublicProfileSet) => void;
+	update: (updater: (value: PublicProfileSet) => PublicProfileSet) => void;
 	add: (profile: PublicProfile) => void;
 	contains: ({ uid, username }: { uid?: string; username?: string }) => boolean;
 	get: ({ uid, username }: { uid?: string; username?: string }) => PublicProfile | null;
 };
 
 function createProfilesStore(): ProfilesStore {
-	const { subscribe, set, update } = writable<PublicProfileMap>({});
+	const { subscribe, set, update } = writable<PublicProfileSet>(new Map());
 
 	const contains = ({ uid, username }: { uid?: string; username?: string }) => {
 		let result = false;
-		subscribe((profiles: PublicProfileMap) => {
-			if (uid) result = Object.keys(profiles).includes(uid);
+		subscribe((profiles: PublicProfileSet) => {
+			if (checkUid(uid)) result = profiles.has(uid as string);
 			if (username)
 				result = Object.values(profiles).filter((profile) => profile.name == username).length > 0;
 		});
@@ -31,10 +32,8 @@ function createProfilesStore(): ProfilesStore {
 	};
 
 	const add = (profile: PublicProfile) => {
-		update((profiles: PublicProfileMap) => {
-			if (profile.uid && !contains({ uid: profile.uid })) {
-				profiles[profile.uid] = profile;
-			}
+		update((profiles: PublicProfileSet) => {
+			if (checkUid(profile.uid)) profiles.set(profile.uid, profile);
 
 			return profiles;
 		});
@@ -44,10 +43,9 @@ function createProfilesStore(): ProfilesStore {
 		if (!contains({ uid, username })) return null;
 		let profile = null;
 
-		subscribe((profiles: PublicProfileMap) => {
-			if (uid) profile = profiles[uid];
-			if (username)
-				profile = Object.values(profiles).filter((profile) => profile.name == username)[0];
+		subscribe((profiles: PublicProfileSet) => {
+			if (uid) profile = profiles.get(uid);
+			if (username) profile = Object.values(profiles).find((profile) => profile.name == username);
 		});
 
 		return profile;
