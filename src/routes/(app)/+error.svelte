83af6d4b571ddac9c73b16b564c0e53ec7613579
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-08-29 14:50:13 +0200

    error page

---
+++ src/routes/(app)/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="flex h-full w-full items-center justify-center">
+	<h2 class="h2">
+		{$page.status}: {$page.error?.message}
+	</h2>
+</div>
