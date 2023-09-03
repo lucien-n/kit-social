cdccd32f6f0cd7f9497c21265015d583178de53e
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-09-03 17:45:42 +0200

    ref: createpost improvements and char limit

--- src/lib/components/post/CreatePost.svelte
+++ src/lib/components/post/CreatePost.svelte
@@ -6,14 +6,20 @@
 	import type { FormEventHandler } from 'svelte/elements';
 	import Avatar from '$comp/Avatar.svelte';
 	import type SocialClient from '$sclient/sclient';
+	import { POST_CHARACTER_LIMIT } from '$lib/utilities/constants';
+	import { onMount } from 'svelte';
 
 	export let sclient: SocialClient;
 	export let form;
 
+	let input: HTMLTextAreaElement;
+
 	let loading = false;
 
-	let content: string = form?.content || '';
+	let content: string = (form?.content as string) || '';
 	let hidePlaceholder = false;
+
+	let current_character_count = 0;
 
 	const handleSubmit: SubmitFunction = ({ formData }) => {
 		loading = true;
@@ -29,8 +35,10 @@
 		};
 	};
 
-	const handleInput: FormEventHandler<HTMLDivElement> = () => {
-		hidePlaceholder = content != '';
+	const handleInput: FormEventHandler<HTMLTextAreaElement> = () => {
+		hidePlaceholder = content.length > 0;
+		current_character_count = content.length;
+		input.style.height = `${input.scrollHeight}px`;
 	};
 </script>
 
@@ -40,29 +48,45 @@
 		<div class="pointer-events-none absolute p-1 text-xl opacity-70" class:hidden={hidePlaceholder}>
 			What's poppin {$profileStore?.name || ''}?
 		</div>
-		<div
-			bind:textContent={content}
-			contenteditable="true"
-			placeholder="true"
-			class="h-20 w-full p-1 text-xl outline-none"
+		<textarea
+			class="h-24 w-full resize-none border-0 border-none bg-transparent p-1 text-xl outline-none outline-0 focus:outline-none focus:outline-0"
+			maxlength={POST_CHARACTER_LIMIT}
+			bind:this={input}
+			bind:value={content}
 			on:input={handleInput}
 		/>
-		<button
-			type="submit"
-			disabled={loading}
-			class="variant-ghost-secondary btn flex items-center gap-1 self-end"
-			name="create-post"
-		>
-			{#if loading}
-				<span class="animate-spin">
-					<Icon icon="mdi:loading" />
-				</span>
-			{:else}
-				<span>
-					<Icon icon="mdi:send" width={20} />
-				</span>
-			{/if}
-			<p>{loading ? 'Sending' : 'Send'}</p></button
-		>
+		<div class="flex items-center gap-3 self-end">
+			<p class="flex gap-1 self-end tracking-wide opacity-70">
+				<span>{current_character_count}</span><span>/</span> <span>{POST_CHARACTER_LIMIT}</span>
+			</p>
+			<button
+				type="submit"
+				disabled={loading}
+				class="variant-ghost-secondary btn flex items-center gap-1"
+				name="create-post"
+			>
+				{#if loading}
+					<span class="animate-spin">
+						<Icon icon="mdi:loading" />
+					</span>
+				{:else}
+					<span>
+						<Icon icon="mdi:send" width={20} />
+					</span>
+				{/if}
+				<p>{loading ? 'Sending' : 'Send'}</p></button
+			>
+		</div>
 	</form>
 </section>
+
+<style>
+	textarea:hover,
+	textarea:active,
+	textarea:focus {
+		outline: 0px !important;
+		appearance: none;
+		-webkit-appearance: none;
+		box-shadow: none !important;
+	}
+</style>
--- src/lib/utilities/constants.ts
+++ src/lib/utilities/constants.ts
@@ -1,1 +1,3 @@
 export const TITLE_PREFIX = 'Social';
+
+export const POST_CHARACTER_LIMIT = 400;
