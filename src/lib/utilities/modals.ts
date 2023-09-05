d235b7c71e6b3f15d2799a19bed915d7a127a45a
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-09-05 17:47:04 +0200

    fix: confirm modal

--- src/lib/utilities/modals.ts
+++ src/lib/utilities/modals.ts
@@ -1,9 +1,10 @@
+import { browser } from "$app/environment";
 import { getModalStore, type ModalSettings } from "@skeletonlabs/skeleton";
 
-const modal_store = getModalStore()
 
 export const modals = {
     confirm: (settings: ModalSettings) => new Promise<boolean>((resolve) => {
-       modal_store.trigger({...settings, response: (r: boolean) => resolve(r)});
+        if (!browser) return
+       getModalStore().trigger({...settings, response: (r: boolean) => resolve(r)});
     }),
 }
