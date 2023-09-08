372e3e0193a7d84de5b025f3ac02403a03665abc
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-09-08 16:45:10 +0200

    ref: improved shortcut component - new kbd component

---
+++ src/lib/components/Kbd.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    export let variant = "variant-ghost-primary"
+</script>
+
+<kbd class="kbd {variant} text-xs font-semibold">
+	<slot />
+</kbd>
--- src/lib/components/Shortcut.svelte
+++ src/lib/components/Shortcut.svelte
@@ -2,37 +2,21 @@
 	import { settingsStore } from '$stores/settings';
 	import { createEventDispatcher } from 'svelte';
 	import type { KeyboardEventHandler } from 'svelte/elements';
+	import Kbd from './Kbd.svelte';
 
 	export let shortcut: TShortcut;
 
 	const dispatch = createEventDispatcher();
 
-	const getShortcutText = () => {
-		let text = '';
+	const getShortcutKeys = () => {
+		const keys: string[] = [];
 
-		let key = shortcut.key.toUpperCase();
-		let ctrl = shortcut.ctrlRequired === true;
-		let shift = shortcut.shiftRequired === true;
-		let alt = shortcut.altRequired === true;
+		if (shortcut.ctrlRequired === true) keys.push('CTRL');
+		if (shortcut.shiftRequired === true) keys.push('ALT');
+		if (shortcut.altRequired === true) keys.push('SHIFT');
+		keys.push(shortcut.key.toUpperCase());
 
-		if (ctrl) {
-			text += 'CTRL';
-			if (shift || alt || key) text += '+';
-		}
-
-		if (alt) {
-			text += 'ALT';
-			if (shift || key) text += '+';
-		}
-
-		if (shift) {
-			text += 'SHIFT';
-			if (key) text += '+';
-		}
-
-		if (key) text += key;
-
-		return text;
+		return keys;
 	};
 
 	$: shortcuts_enabled = settingsStore.isEnabled('shortcuts');
@@ -52,5 +36,12 @@
 <svelte:window on:keypress={handleKeypress} />
 
 {#if shortcuts_enabled}
-	<kbd class="kbd variant-ghost-primary text-xs font-semibold">{getShortcutText()}</kbd>
+	<span class="flex items-center gap-1">
+		{#each getShortcutKeys() as key, index}
+			<Kbd>{key}</Kbd>
+			{#if index < getShortcutKeys().length - 1}
+				+
+			{/if}
+		{/each}
+	</span>
 {/if}
